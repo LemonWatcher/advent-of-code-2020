ba3f175fabdceb05f16f01533e75c3291e5bf5f0
Thom Lawrence <thom@lemonwatcher.com>
2020-12-01 13:30:25 +0000

    Added solution to first part of 1st puzzle.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Advent of Code 2020
+
+My repository of solutions to 2020's advent of code, in Clojure.
+
+## Structure
+
+Each namespace is named for a day, tests are kept alongside solutions,
+and Marginalia docs are kept in the doc folder.
+
+## License
+
+Copyright © 2020 Thom Lawrence
+
+This code is ditributed under the terms of the
+[GNU Affero General Public License, version 3](https://www.gnu.org/licenses/agpl-3.0.en.html).
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject advent-of-code-2020 "0.1.0"
+  :description "Advent of Code 2020"
+  :url "https://github.com/lemonwatcher/advent-of-code-2020"
+  :license {:name "GNU Affero General Public License Version 3"
+            :url "https://www.gnu.org/licenses/agpl-3.0.en.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/math.combinatorics "0.1.6"]]
+  :plugins [[lein-marginalia "0.9.1"]]
+  :repl-options {:init-ns advent-of-code-2020.core})
