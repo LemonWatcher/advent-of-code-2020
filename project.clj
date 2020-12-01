3e729d5fae68d7f895c03365eccfd42f14a19c74
Thom Lawrence <thom@lemonwatcher.com>
2020-12-01 13:44:34 +0000

    Added part two of 1st puzzle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject advent-of-code-2020 "0.1.0"
+(defproject advent-of-code-2020 "0.1.1"
   :description "Advent of Code 2020"
   :url "https://github.com/lemonwatcher/advent-of-code-2020"
   :license {:name "GNU Affero General Public License Version 3"
