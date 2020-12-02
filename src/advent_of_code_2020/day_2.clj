062a24c95fff8a40b4d2025b34db0e749128c38a
Thom Lawrence <thom@lemonwatcher.com>
2020-12-02 11:07:39 +0000

    Refactor to allow policy to be specified.

--- src/advent_of_code_2020/day_2.clj
+++ src/advent_of_code_2020/day_2.clj
@@ -9,7 +9,7 @@
 ;; Read lines of password policies and passwords, and check i f the
 ;; password matches the policy. Each line is of the form:
 ;;
-;;     x-y c: password
+;;     a-b c: password
 ;;
 ;; Where x is the minimum required, and y the maximum allowed, number
 ;; of instances of character c in password.
@@ -34,9 +34,9 @@
 
 (s/def ::password-line
   (s/cat
-   :min ::bound
+   :a ::bound
    :sep ::separator
-   :max ::bound
+   :b ::bound
    :sep ::separator
    :char char?
    :sep ::separator
@@ -51,36 +51,36 @@
   "Parse a single line of input in `line` into a map holding the `:min`,
   `:max`, `:char` and `:password`."
   [line]
-  (let [{:keys [min max char password]} (s/conform ::password-line (seq line))]
-    {:min (coerce-int min)
-     :max (coerce-int max)
+  (let [{:keys [a b char password]} (s/conform ::password-line (seq line))]
+    {:a (coerce-int a)
+     :b (coerce-int b)
      :char char
      :password password ;; don't bother coercing to a string
      }))
 
-(defn matches-rule?
+(defn matches-seld-rental-policy?
   "Check if the password matches the associated rule."
-  [{:keys [min max char password]}]
+  [{:keys [a b char password]}]
   (let [freqs (frequencies password)]
-    (<= min (freqs char 0) max)))
+    (<= a (freqs char 0) b)))
 
 (defn valid-line?
   "Does the line contain a valid password for its rule?"
-  [line]
-  (matches-rule? (parse-line line)))
+  [policy line]
+  (policy (parse-line line)))
 
 (defn count-valid-passwords
   "Counts the number of valid passwords in the input."
-  []
+  [policy]
   (->> (load-input)
        (split-lines)
-       (filter valid-line?)
+       (filter (partial valid-line? policy))
        (count)))
 
 (deftest valid-line-tests
-  (is (valid-line? "1-3 a: abcde"))
-  (is (not (valid-line? "1-3 b: cdefg")))
-  (is (valid-line? "2-9 c: ccccccccc")))
+  (is (valid-line? matches-seld-rental-policy? "1-3 a: abcde"))
+  (is (not (valid-line? matches-seld-rental-policy? "1-3 b: cdefg")))
+  (is (valid-line? matches-seld-rental-policy? "2-9 c: ccccccccc")))
 
 (deftest count-valid-passwords-test
-  (is (= 628 (count-valid-passwords))))
+  (is (= 628 (count-valid-passwords matches-seld-rental-policy?))))
