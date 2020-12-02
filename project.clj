d97ae5b2439b84cb25ea7a5f239ac1e06ebf68d7
thom <thom@penguin>
2020-12-02 03:06:48 +0000

    Added recursive solution for the 1st puzzle.

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject advent-of-code-2020 "0.1.1"
+(defproject advent-of-code-2020 "0.1.2"
   :description "Advent of Code 2020"
   :url "https://github.com/lemonwatcher/advent-of-code-2020"
   :license {:name "GNU Affero General Public License Version 3"
             :url "https://www.gnu.org/licenses/agpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/math.combinatorics "0.1.6"]]
+                 [org.clojure/math.combinatorics "0.1.6"]
+                 [shams/priority-queue "0.1.2"]
+                 [criterium "0.4.6"]]
   :plugins [[lein-marginalia "0.9.1"]]
-  :repl-options {:init-ns advent-of-code-2020.core})
+  :repl-options {:init-ns advent-of-code-2020})
